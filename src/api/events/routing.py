5b5ef436ab48b773d14649c6abf5683e25a4c5c7
Abhishek Hiremath <131762197+Abhi-DevHub@users.noreply.github.com>
2025-06-16 16:09:37 +0530

    Update event retrieval notebook and API routing to handle specific event IDs

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -5,7 +5,14 @@ router = APIRouter()
 @router.get("/")
 def read_events():
     return {
+            # lists
             "items":[1,2,3,4,5],
             "message":"Events fetched successfully"
            }
-    
+
+@router.get("/{event_id}")
+def get_event(event_id: int):
+    return {
+            # single row
+            "event_id": event_id,
+           }    

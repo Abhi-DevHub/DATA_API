83e2be19401f8f906df46096b822ad210079c9e3
Abhishek Hiremath <131762197+Abhi-DevHub@users.noreply.github.com>
2025-06-16 15:49:21 +0530

    Implement API events router and add verification notebook for event retrieval

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
+from api.events.routing import router as events_router
 
 app = FastAPI()
+app.include_router(events_router, prefix="/api/events")
+
 # RESTAPI 
 @app.get("/")
 def read_root():
